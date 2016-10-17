3f3ee65fe9285c7465355ddd507a2617f0b3af61
s20151104682 <s20151104682@mac21.local>
2016-10-17 19:40:25 +0800

    Initial Commit

---
+++ 关歆/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  关歆
+//
+//  Created by s20151104682 on 16/10/17.
+//  Copyright © 2016年 s20151104682. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
